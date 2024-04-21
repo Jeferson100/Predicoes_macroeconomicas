bc140a1215a74253389268f8c689ee07bc224af2
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2024-04-21 23:57:35 +0000

    Modificando as pastas dos arquivos

---
+++ test_economics/test_coleta_economic.py
@@ -0,0 +1,36 @@
+from economic_brazil.coleta_economic_brazil import (
+    dados_bcb,
+    dados_ibge_codigos,
+    dados_ibge_link,
+    metas_inflacao,
+    dados_expectativas_focus,
+)
+
+# write tests for tratando_dados_bcb
+def test_tratando_dados_bcb():
+    dados = dados_bcb({"selic": 4189}, "2000-01-01")
+    assert len(dados) > 0
+
+
+##write tests for dados_ibge_codigos
+def test_dados_ibge_codigos():
+    dados = dados_ibge_codigos()
+    assert len(dados) > 0
+
+
+##write tests for dados_ibge_link
+def test_dados_ibge_link():
+    dados = dados_ibge_link()
+    assert len(dados) > 0
+
+
+##write tests for metas_inflacao
+def test_metas_inflacao():
+    dados = metas_inflacao()
+    assert len(dados) > 0
+
+
+##write tests for dados_expectativas_focus
+def test_dados_expectativas_focus():
+    dados = dados_expectativas_focus()
+    assert len(dados) > 0
